a77cc1805fa311c155b6ab546b8899cbbefd6e8e
afloren1 <bluemoon66063@gmail.com>
2024-08-09 17:15:16 -0400

    added code to emotion_detection.py to format the output

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,7 +6,20 @@ def emotion_detection(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json=myobj, headers=headers)
-    formatted_response = json.loads(response.text) 
-    label = formatted_response['emotionPredictions']['label'] 
-    score = formatted_response['emotionPredictions']['score'] 
-    return {'label': label, 'score': score} 
+    data = response.json()
+    emotion_data = data['emotionPredictions'][0]['emotion']
+    anger_score = emotion_data['anger']
+    disgust_score = emotion_data['disgust']
+    fear_score = emotion_data['fear']
+    joy_score = emotion_data['joy']
+    sadness_score = emotion_data['sadness']
+    dominant_emotion = max(emotion_data, key=emotion_data.get)
+    formatted_response = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
+    return formatted_response
